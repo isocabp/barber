f1f9bbddc8d8d1fc771ae755b84501cd7e4c3b59
isocabp <isabellab.ferreira5@gmail.com>
2025-08-19 19:32:27 -0300

    feat: add booking item

--- barber/src/app/page.tsx
+++ barber/src/app/page.tsx
@@ -1,8 +1,11 @@
-import Header from "./_components/header"
-import { Input } from "./_components/ui/input"
-import { Button } from "./_components/ui/button"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
+import Header from "./_components/header"
+import { Badge } from "./_components/ui/badge"
+import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
+import { Input } from "./_components/ui/input"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -10,16 +13,17 @@ const Home = () => {
       {/* Header */}
       <Header />
       <div className="p-5">
+        {/* Título  */}
         <h2 className="text-xl font-bold">Olá, Isabella!</h2>
         <p>Terça-feira, 19 de agosto.</p>
-
+        {/* Busca  */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua busca..." />
           <Button>
             <SearchIcon />
           </Button>
         </div>
-
+        {/* Banner  */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -28,6 +32,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+        {/* AGENDAMENTO  */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/45331760-899c-4b4b-910e-e00babb6ed81-16q.png"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">19</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
