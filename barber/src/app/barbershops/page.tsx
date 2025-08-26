c0eee6f19b6d9acb7496573ed4723f85dc8e9804
isocabp <isabellab.ferreira5@gmail.com>
2025-08-26 00:51:44 -0300

    feat: ensure barbershops can be searched by title and service

--- barber/src/app/barbershops/page.tsx
+++ barber/src/app/barbershops/page.tsx
@@ -1,23 +1,43 @@
-import { db } from "../_lib/prisma"
 import BarbershopItem from "../_components/barbershop-item"
 import Header from "../_components/header"
 import Search from "../_components/search"
+import { db } from "../_lib/prisma"
 
 interface BarbershopsPageProps {
   searchParams: {
-    search?: string
+    title?: string
+    service?: string
   }
 }
 
-const BarbershopsPages = async ({ searchParams }: BarbershopsPageProps) => {
+const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
   const barbershops = await db.barbershop.findMany({
     where: {
-      name: {
-        contains: searchParams?.search,
-        mode: "insensitive",
-      },
+      OR: [
+        searchParams?.title
+          ? {
+              name: {
+                contains: searchParams?.title,
+                mode: "insensitive",
+              },
+            }
+          : {},
+        searchParams.service
+          ? {
+              services: {
+                some: {
+                  name: {
+                    contains: searchParams.service,
+                    mode: "insensitive",
+                  },
+                },
+              },
+            }
+          : {},
+      ],
     },
   })
+
   return (
     <div>
       <Header />
@@ -26,7 +46,8 @@ const BarbershopsPages = async ({ searchParams }: BarbershopsPageProps) => {
       </div>
       <div className="px-5">
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-          Resultados para &quot;{searchParams.search}&quot;
+          Resultados para &quot;{searchParams?.title || searchParams?.service}
+          &quot;
         </h2>
         <div className="grid grid-cols-2 gap-4">
           {barbershops.map((barbershop) => (
@@ -38,4 +59,4 @@ const BarbershopsPages = async ({ searchParams }: BarbershopsPageProps) => {
   )
 }
 
-export default BarbershopsPages
+export default BarbershopsPage
